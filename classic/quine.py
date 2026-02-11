87e469917d139dada2979fb76f1333995ebe2bee
BitCook <mist@outlook.in>
2026-02-11 16:03:24 +0800

    Fix Quine source files to pass strict self-replication tests

--- classic/quine.py
+++ classic/quine.py
@@ -1,6 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Python 3 Quine
-"""
 s='s=%r;print(s%%s)';print(s%s)
--- classic/quine_short.py
+++ classic/quine_short.py
@@ -1,6 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Python Minimal Quine (29 chars)
-"""
 _='_=%r;print(_%%_)';print(_%_)
